fd9d21fb850bcb4bcd2d0b31183b1537ee3affa1
github username <github email address>
2020-12-30 13:06:22 -0800

    add bars and x icon for responsive screen

--- frontend-react-redux/src/components/navbar/index.js
+++ frontend-react-redux/src/components/navbar/index.js
@@ -1,20 +1,44 @@
 import React, { useState } from "react";
-import { FaTimes, FaBars } from "react-icons/fa";
+import { FaTimes, FaBars, FaArrowDown } from "react-icons/fa";
 import { NavLink } from "react-router-dom";
 import "./Navbar.css";
 import logo from "../../images/logo.png";
+import Dropdown from "../dropdown";
+import Button from "../button";
 
 function Navbar() {
   const [clicked, setClicked] = useState(false);
+  const [closeMenu, setCloseMenu] = useState(false);
+  const [dropdown, setDropdown] = useState(false);
+
   return (
     <nav className="navbar">
       <NavLink to="/">
         <img className="navbar-logo" src={logo} alt="logo" />
       </NavLink>
-      <div className="menu-icon">{clicked ? <FaBars /> : <FaTimes />}</div>
+      <div className="menu-icon">
+        {clicked ? (
+          <FaBars onClick={() => setClicked(!clicked)} />
+        ) : (
+          <FaTimes onClick={() => setClicked(!clicked)} />
+        )}
+      </div>
       <ul className={clicked ? "nav-menu active" : "nav-menu"}>
         <li className="nav-item">
-          <NavLink className="nav-link" to="/start-quiz">
+          <NavLink
+            className="nav-link"
+            onClick={() => setCloseMenu(!closeMenu)}
+            to="/"
+          >
+            Home
+          </NavLink>
+        </li>
+        <li className="nav-item">
+          <NavLink
+            className="nav-link"
+            onClick={() => setCloseMenu(!closeMenu)}
+            to="/start-quiz"
+          >
             Start Quiz
           </NavLink>
         </li>
@@ -24,11 +48,22 @@ function Navbar() {
           </NavLink>
         </li>
         <li className="nav-item">
-          <NavLink className="nav-link" to="/#">
-            View/Edit Questions
+          <NavLink
+            className="nav-link arrow-down"
+            to="/#"
+            onClick={() => setDropdown(!dropdown)}
+          >
+            Questions <FaArrowDown />
+          </NavLink>
+          {dropdown && <Dropdown />}
+        </li>
+        <li className="nav-item">
+          <NavLink className="nav-link-dropdown" to="/add-question">
+            Add Question
           </NavLink>
         </li>
       </ul>
+      <Button />
     </nav>
   );
 }
