04e84f58c5f0b17a2baec27ad05239f496ac8003
github username <github email address>
2020-12-29 20:04:19 -0800

    add FaBars and FaTimes from react icons

--- frontend-react-redux/src/components/navbar/index.js
+++ frontend-react-redux/src/components/navbar/index.js
@@ -1,32 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
+import { FaTimes, FaBars } from "react-icons/fa";
 import { NavLink } from "react-router-dom";
 import "./Navbar.css";
+import logo from "../../images/logo.png";
 
 function Navbar() {
+  const [clicked, setClicked] = useState(false);
   return (
     <nav className="navbar">
-      <NavLink className="navbar logo" to="/">
-        Navbar
+      <NavLink to="/">
+        <img className="navbar-logo" src={logo} alt="logo" />
       </NavLink>
-      <div className="nav" id="navbarText">
-        <ul className="nav-menu">
-          <li className="nav-item">
-            <NavLink className="nav-link" to="/start-quiz">
-              Start Quiz
-            </NavLink>
-          </li>
-          <li className="nav-item">
-            <NavLink className="nav-link" to="/high-score">
-              High Score
-            </NavLink>
-          </li>
-          <li className="nav-item">
-            <NavLink className="nav-link" to="/#">
-              View/Edit Questions
-            </NavLink>
-          </li>
-        </ul>
-      </div>
+      <div className="menu-icon">{clicked ? <FaBars /> : <FaTimes />}</div>
+      <ul className={clicked ? "nav-menu active" : "nav-menu"}>
+        <li className="nav-item">
+          <NavLink className="nav-link" to="/start-quiz">
+            Start Quiz
+          </NavLink>
+        </li>
+        <li className="nav-item">
+          <NavLink className="nav-link" to="/high-score">
+            High Score
+          </NavLink>
+        </li>
+        <li className="nav-item">
+          <NavLink className="nav-link" to="/#">
+            View/Edit Questions
+          </NavLink>
+        </li>
+      </ul>
     </nav>
   );
 }
