abe1f609d3f22a7232619b912a0e744294b0e4eb
github username <github email address>
2020-12-30 16:52:42 -0800

    update route path and component

--- frontend-react-redux/src/App.js
+++ frontend-react-redux/src/App.js
@@ -1,10 +1,19 @@
 import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Quiz from "./features/quiz";
+import Home from "./features/home";
+import Navbar from "./components/navbar";
 
 function App() {
   return (
     <div className="App">
-      <Quiz />
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/start-quiz" exact component={Quiz} />
+        </Switch>
+      </Router>
     </div>
   );
 }
