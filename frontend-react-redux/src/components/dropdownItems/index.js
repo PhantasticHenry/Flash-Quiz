da29564d0170261e080886b833d44106c4bb75eb
github username <github email address>
2020-12-30 17:04:56 -0800

    create dropdown items for dropdown menu

---
+++ frontend-react-redux/src/components/dropdownItems/index.js
@@ -0,0 +1,22 @@
+export const DropdownItems = [
+  {
+    title: "Add Question",
+    path: "/add-question",
+    cName: "dropdown-link",
+  },
+  {
+    title: "Edit Question",
+    path: "/edit-question",
+    cName: "dropdown-link",
+  },
+  {
+    title: "Remove Question",
+    path: "/remove-question",
+    cName: "dropdown-link",
+  },
+  {
+    title: "Flashcards",
+    path: "/flashcards",
+    cName: "dropdown-link",
+  },
+];
