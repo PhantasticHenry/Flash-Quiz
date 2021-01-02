07f0fa03a5697e44508410e3a3f8447ed0c56c75
github username <github email address>
2021-01-01 21:13:14 -0800

    add link to refresh page and start quiz over

--- frontend-react-redux/src/components/result/index.js
+++ frontend-react-redux/src/components/result/index.js
@@ -1,10 +1,14 @@
 import React from "react";
+import "./Result.css";
+import { NavLink } from "react-router-dom";
 
 function Result({ correct, incorrect }) {
   return (
-    <div>
-      Awesome! You answered {correct} correctly and {incorrect} incorrectly out
-      of {correct + incorrect} total quesitons.
+    <div className="result">
+      {`Excellent! You got ${correct} out of ${correct + incorrect}.`}
+      <NavLink to="/start-quiz" onClick={() => window.location.reload()}>
+        Try Again
+      </NavLink>
     </div>
   );
 }
