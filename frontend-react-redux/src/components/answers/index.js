ab02d6e2ef960e537d41f41ae7a57ded59c70c3f
github username <github email address>
2021-01-01 21:16:04 -0800

    wrapped answers with li tags with eventlistener, key, value, className

--- frontend-react-redux/src/components/answers/index.js
+++ frontend-react-redux/src/components/answers/index.js
@@ -1,13 +1,19 @@
 import React from "react";
+import "./Answers.css";
 
 function Answers({ answers, handleClick }) {
   const options = answers.map((answer, i) => (
-    <li onClick={handleClick} key={i} value={answer}>
+    <li
+      onClick={handleClick}
+      key={i}
+      value={answer}
+      className={`option-${i + 1}`}
+    >
       {answer}
     </li>
   ));
 
-  return <div>{options}</div>;
+  return <div className="answers">{options}</div>;
 }
 
 export default Answers;
