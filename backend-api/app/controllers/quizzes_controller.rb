f2c814c8edeade76cbe07b4217666a8913fb9916
github username <github email address>
2021-01-02 16:18:11 -0800

    index create actions and private quiz_params method created

--- backend-api/app/controllers/quizzes_controller.rb
+++ backend-api/app/controllers/quizzes_controller.rb
@@ -1,2 +1,22 @@
 class QuizzesController < ApplicationController
+
+  def index 
+    @quizzes = Quiz.all 
+    render json: @quizzes
+  end
+
+  def create 
+    quiz = Quiz.new(quiz_params)
+    if quiz.save! 
+      render json: quiz
+    else 
+      render json: {error: "Something went wrong during creation"}
+    end
+  end
+
+  private 
+  
+  def quiz_params
+    params.require(:quiz).permit(:player, :high_score)
+  end
 end
