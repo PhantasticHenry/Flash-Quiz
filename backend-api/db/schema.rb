5d310d61b0aa34bfa05d9ec196e989bc9d785b14
github username <github email address>
2021-01-02 16:24:13 -0800

    ran db:migrate

---
+++ backend-api/db/schema.rb
@@ -0,0 +1,45 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `rails
+# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2021_01_03_002201) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "flashcards", force: :cascade do |t|
+    t.string "category"
+    t.string "question"
+    t.string "correct_answer"
+    t.string "incorrect_answers", default: [], array: true
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "quiz_flashcards", force: :cascade do |t|
+    t.bigint "quiz_id", null: false
+    t.bigint "flashcard_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["flashcard_id"], name: "index_quiz_flashcards_on_flashcard_id"
+    t.index ["quiz_id"], name: "index_quiz_flashcards_on_quiz_id"
+  end
+
+  create_table "quizzes", force: :cascade do |t|
+    t.string "player", default: "Player"
+    t.integer "high_score", default: 0
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  add_foreign_key "quiz_flashcards", "flashcards"
+  add_foreign_key "quiz_flashcards", "quizzes"
+end
