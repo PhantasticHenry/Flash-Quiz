b6f7561d6ffa2cc61fc4ed3c2e3adea6c7b9066b
github username <github email address>
2021-01-02 16:48:13 -0800

    add CRUD actions

--- backend-api/app/controllers/flashcards_controller.rb
+++ backend-api/app/controllers/flashcards_controller.rb
@@ -1,3 +1,47 @@
 class FlashcardsController < ApplicationController
+  before_action :set_flashcard
   
+  def index 
+    @flashcards = Flashcard.all 
+    render json: @flashcards
+  end
+
+  def create 
+    flashcard = Flashcard.new(flashcard_params)
+    if flashcard.save! 
+      render json: flashcard
+    else 
+      render json: {error: "Something went wrong during creation"}
+  end
+
+  def edit 
+    @flashcard
+  end
+
+  def update 
+    if @flashcard.update(flashcard_params)
+      render json: @flashcard
+    else 
+      render json: render json: {error: "Something went wrong during update"}
+    end
+  end
+
+  def destroy 
+    if @flashcard.destroy
+      render json: @flashcards
+    else 
+      render json: {error: "Something went wrong could not delete flashcard"}
+    end
+  end
+
+  
+  private
+  
+  def flashcard_params
+    params.require(:flashcard).permit(:category, :question, :correct_answer, :incorrect_answers)
+  end
+
+  def set_flashcard 
+    @flashcard = Flashcard.find_by(id: params[:id])
+  end
 end
