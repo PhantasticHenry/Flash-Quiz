b94fca9b7e4ff9592bb7496c7076e9efe6ceddf0
github username <github email address>
2021-01-03 15:15:37 -0800

    action types created

---
+++ frontend-react-redux/src/actions/actionTypes.js
@@ -0,0 +1,8 @@
+export const FETCH_FLASHCARDS = "FETCH_FLASHCARDS";
+export const ADD_FLASHCARD = "ADD_FLASHCARD";
+export const EDIT_FLASHCARD = "EDIT_FLASHCARD";
+export const REMOVE_FLASHCARD = "REMOVE_FLASHCARD";
+
+export const FETCH_QUIZZES = "FETCH_QUIZZES";
+export const ADD_QUIZ = "ADD_QUIZ";
+export const EDIT_QUIZ = "EDIT_QUIZ";
