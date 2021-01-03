538ede1c7aab81e18112df730e359495f792f3e5
github username <github email address>
2021-01-02 16:34:16 -0800

    add associations

--- backend-api/app/models/flashcard.rb
+++ backend-api/app/models/flashcard.rb
@@ -1,2 +1,4 @@
 class Flashcard < ApplicationRecord
+  has_many :quiz_flashcards
+  has_many :quizzes, through: :quiz_flashcards
 end
--- backend-api/app/models/quiz.rb
+++ backend-api/app/models/quiz.rb
@@ -1,3 +1,4 @@
 class Quiz < ApplicationRecord
-  
+  has_many :quiz_flashcards
+  has_many :flashcards, through: :quiz_flashcards
 end
