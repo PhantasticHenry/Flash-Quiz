0e489b6f01915873405c7065c261e2bc34ad1a83
github username <github email address>
2021-01-03 19:17:14 -0800

    remove before_action and add category to strong params

--- backend-api/app/controllers/quizzes_controller.rb
+++ backend-api/app/controllers/quizzes_controller.rb
@@ -1,17 +1,14 @@
 class QuizzesController < ApplicationController
-before_action :set_flashcards
+
   def index 
     @quizzes = Quiz.all 
     render json: @quizzes
   end
 
-  def create(category)
+  def create
     quiz = Quiz.new(quiz_params)
     if quiz.save! 
-      # flashcards.map do |flashcard|
-      #   QuizFlashcard.create(quiz_id: quiz.id, flashcard_id: flashcard.id )
-      # end
-      select_category(category)
+      @flashcards = Flashcard.select { |flashcard| flashcard.category === quiz.category }
       @flashcards.map do |flashcard|
         QuizFlashcard.create(quiz_id: quiz.id, flashcard_id: flashcard.id)
       end
@@ -24,14 +21,6 @@ before_action :set_flashcards
   private 
   
   def quiz_params
-    params.require(:quiz).permit(:player, :high_score)
-  end
-
-  def set_flashcards 
-    @flashcards = Flashcard.all
-  end
-
-  def select_category(selection)
-    @flashcards = Flashcard.select { |flashcard| flashcard.category === selection}
+    params.require(:quiz).permit(:player, :high_score, :category)
   end
 end
