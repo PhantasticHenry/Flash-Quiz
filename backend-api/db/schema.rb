75ef50dcbbb6f07e3877a7c8f7f18462e34072fb
github username <github email address>
2021-01-03 19:06:16 -0800

    add category column to quiz

--- backend-api/db/schema.rb
+++ backend-api/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_03_002201) do
+ActiveRecord::Schema.define(version: 2021_01_04_030428) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 2021_01_03_002201) do
     t.integer "high_score", default: 0
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "category"
   end
 
   add_foreign_key "quiz_flashcards", "flashcards"
