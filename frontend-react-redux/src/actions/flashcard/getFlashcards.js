a2b78925899c90b9ce5168c7311c131a0c2a1e31
github username <github email address>
2021-01-04 07:54:31 -0800

    create action to fetch flashcards

---
+++ frontend-react-redux/src/actions/flashcard/getFlashcards.js
@@ -0,0 +1,12 @@
+import * as types from "../actionTypes";
+
+export const getFlashcards = () => {
+  return (dispatch) => {
+    fetch(types.api + "flashcards")
+      .then((res) => res.json())
+      .then((flashcards) =>
+        dispatch({ type: types.FETCH_FLASHCARDS, payload: flashcards })
+      )
+      .catch((error) => console.log("**ERROR**", error));
+  };
+};
