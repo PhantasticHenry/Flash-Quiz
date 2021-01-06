7b96b5df92315c1fd772adbb44e11ef2ff00d90d
github username <github email address>
2021-01-05 22:43:50 -0800

    destructure and rendered category prop

--- frontend-react-redux/src/components/category/index.js
+++ frontend-react-redux/src/components/category/index.js
@@ -2,7 +2,11 @@ import React from "react";
 import "./Category.css";
 
 function Category({ category }) {
-  return <button className="btn-category">{category}</button>;
+  return (
+    <button className="btn-category" name={category}>
+      {category}
+    </button>
+  );
 }
 
 export default Category;
