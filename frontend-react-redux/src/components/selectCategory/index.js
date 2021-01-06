d54f372756af4f45d02f39cb5127924660ec541f
github username <github email address>
2021-01-05 22:48:45 -0800

    destructure props and function to set category created

--- frontend-react-redux/src/components/selectCategory/index.js
+++ frontend-react-redux/src/components/selectCategory/index.js
@@ -1,19 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
 import "./SelectCategory.css";
 import Category from "../category";
 
-function SelectCategory() {
+function SelectCategory({ closeCategories, category }) {
   const categories = useSelector((state) => state.flashcards).map(
     (f) => f.category
   );
 
   const uniqCat = [...new Set(categories)];
 
+  const handleClick = (e) => {
+    closeCategories();
+    category(e);
+  };
+
   return (
     <div className="select-category">
       <h3 className="select">Please Select a Category</h3>
-      <Category category={uniqCat} />
+      {/* <Link to={{pathname: '/start-quiz', }} onClick={(e) => handleClick(e)}> */}
+      <Link to="/start-quiz" onClick={(e) => handleClick(e)}>
+        <Category category={uniqCat} />
+      </Link>
     </div>
   );
 }
