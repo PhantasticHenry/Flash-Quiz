ba43cdcedf9e495644483638c8c1f5f1f7c08b11
github username <github email address>
2021-01-07 18:33:33 -0800

    startQuiz function to dispatch post request

---
+++ frontend-react-redux/src/actions/quiz/startQuiz.js
@@ -0,0 +1,24 @@
+import * as types from "../actionTypes";
+
+export const startQuiz = (quiz) => {
+  return (dispatch) => {
+    return fetch(types.api + "quizzes", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        quiz: {
+          player: quiz.player,
+          high_score: quiz.high_score,
+          category: quiz.category,
+        },
+      }),
+    })
+      .then((res) => res.json())
+      .then((quiz) => {
+        quiz.error
+          ? alert(quiz.details)
+          : dispatch({ type: types.START_QUIZ, payload: quiz });
+      })
+      .catch((error) => alert(error));
+  };
+};
