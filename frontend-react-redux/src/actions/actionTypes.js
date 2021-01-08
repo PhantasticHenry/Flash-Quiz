f8afe5623e9e49a9262405fa14c156708b8c220b
github username <github email address>
2021-01-07 18:31:28 -0800

    changed addQuiz to  startQuiz

--- frontend-react-redux/src/actions/actionTypes.js
+++ frontend-react-redux/src/actions/actionTypes.js
@@ -5,6 +5,7 @@ export const REMOVE_FLASHCARD = "REMOVE_FLASHCARD";
 
 export const FETCH_QUIZZES = "FETCH_QUIZZES";
 export const ADD_QUIZ = "ADD_QUIZ";
+export const START_QUIZ = "START_QUIZ";
 export const EDIT_QUIZ = "EDIT_QUIZ";
 
 export const FETCH_QUIZ_FLASHCARDS = "FETCH_QUIZ_FLASHCARDS";
