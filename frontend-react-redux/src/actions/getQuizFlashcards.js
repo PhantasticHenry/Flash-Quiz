b510c3f881afce349c11ac6c5a9e1fc96c4d75a9
github username <github email address>
2021-01-10 22:39:27 -0800

    fetch quiz_flashcards and filter quizID

--- frontend-react-redux/src/actions/getQuizFlashcards.js
+++ frontend-react-redux/src/actions/getQuizFlashcards.js
@@ -1,14 +1,17 @@
 import * as types from "./actionTypes";
 
-const quizFlashcardsAPI = "http://localhost:3000/quiz_flashcards";
-
-export const getQuizFlashcards = () => {
+export const getQuizFlashcards = (quizID) => {
   return (dispatch) => {
-    fetch(quizFlashcardsAPI)
+    fetch(types.api + "quiz_flashcards")
       .then((res) => res.json())
       .then((quizFlashcards) =>
-        dispatch({ type: types.FETCH_QUIZ_FLASHCARDS, payload: quizFlashcards })
+        dispatch({
+          type: types.FETCH_QUIZ_FLASHCARDS,
+          payload: quizFlashcards.filter(
+            (flashcard) => flashcard.quiz_id === quizID
+          ),
+        })
       )
-      .catch((err) => alert(err));
+      .catch((err) => console.log("ERROR: ", err));
   };
 };
