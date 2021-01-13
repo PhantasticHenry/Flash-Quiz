5d86f90006ea5a0cde0aae998aebfd20ea19f7e0
github username <github email address>
2021-01-12 17:19:53 -0800

    FETCH_QUIZ constant added to action types

--- frontend-react-redux/src/actions/actionTypes.js
+++ frontend-react-redux/src/actions/actionTypes.js
@@ -4,6 +4,7 @@ export const EDIT_FLASHCARD = "EDIT_FLASHCARD";
 export const REMOVE_FLASHCARD = "REMOVE_FLASHCARD";
 
 export const FETCH_QUIZZES = "FETCH_QUIZZES";
+export const FETCH_QUIZ = "FETCH_QUIZ";
 export const ADD_QUIZ = "ADD_QUIZ";
 export const START_QUIZ = "START_QUIZ";
 export const EDIT_QUIZ = "EDIT_QUIZ";
