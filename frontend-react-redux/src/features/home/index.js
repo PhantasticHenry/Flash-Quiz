10f034c56d2feefa869d63e555b64f06291e94b0
github username <github email address>
2021-01-12 17:23:34 -0800

    after componentDidMount dispatch to fetch quizzes

--- frontend-react-redux/src/features/home/index.js
+++ frontend-react-redux/src/features/home/index.js
@@ -2,11 +2,16 @@ import React, { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import "./Home.css";
 import { getFlashcards } from "../../actions/flashcard/getFlashcards";
+import { getQuizzes } from "../../actions/quiz/getQuizzes";
 
 function Home() {
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(getFlashcards());
+  }, []);
+
+  useEffect(() => {
+    dispatch(getQuizzes());
   }, []);
 
   return (
