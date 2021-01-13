62aabe11df83015c38855c9cd41d373627ab100b
github username <github email address>
2021-01-13 00:43:45 -0800

    update added case UPDATE_QUIZ to update current quiz

--- frontend-react-redux/src/reducers/quizReducer.js
+++ frontend-react-redux/src/reducers/quizReducer.js
@@ -2,6 +2,16 @@ export const quizReducer = (state = [], action) => {
   switch (action.type) {
     case "FETCH_QUIZZES":
       return action.payload;
+
+    case "FETCH_QUIZ":
+      return action.payload;
+
+    case "START_QUIZ":
+      return [...state, action.payload];
+
+    case "UPDATE_QUIZ":
+      return [...state, action.payload];
+
     default:
       return state;
   }
