30c70ce74ea975a8721a25c79a7ac4643c1e9742
github username <github email address>
2021-01-16 15:14:38 -0800

    useRef and set flashcard height and height style upon size

--- frontend-react-redux/src/components/flashcard/index.js
+++ frontend-react-redux/src/components/flashcard/index.js
@@ -1,8 +1,18 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import "../../features/flashcards/Flashcards.css";
 
 function Flashcard({ flashcard }) {
   const [flip, setFlip] = useState(false);
+  const [height, setHeight] = useState("initial");
+
+  const cardFront = useRef();
+  const cardBack = useRef();
+
+  function setMaxHeight() {
+    const frontHeight = cardFront.current.getBoundingClientRect().height;
+    const backHeight = cardBack.current.getBoundingClientRect().height;
+    setHeight(Math.max(frontHeight, backHeight, 100));
+  }
 
   const answersArray = [
     ...flashcard.incorrect_answers,
@@ -17,19 +27,30 @@ function Flashcard({ flashcard }) {
     );
   });
 
+  useEffect(setMaxHeight, [
+    flashcard.question,
+    flashcard.correct_answer,
+    answers,
+  ]);
+
+  useEffect(() => {
+    window.addEventListener("resize", setMaxHeight);
+    return () => window.removeEventListener("resize", setMaxHeight);
+  }, []);
+
   return (
     <div
       className={`flashcard ${flip ? "flip" : ""}`}
+      style={{ height: height }}
       onClick={() => setFlip(!flip)}
     >
-      {flip ? (
-        <div className="front">
-          {flashcard.question}
-          <ul className="answers">{answers}</ul>
-        </div>
-      ) : (
-        <div className="back">{flashcard.correct_answer}</div>
-      )}
+      <div className="front" ref={cardFront}>
+        {flashcard.question}
+        <ul className="answers">{answers}</ul>
+      </div>
+      <div className="back" ref={cardBack}>
+        {flashcard.correct_answer}
+      </div>
     </div>
   );
 }
