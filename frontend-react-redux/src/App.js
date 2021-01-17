6483e26358c9723a07ba976227ff33f6d269765c
github username <github email address>
2021-01-17 14:02:50 -0800

    Flashcard Route added

--- frontend-react-redux/src/App.js
+++ frontend-react-redux/src/App.js
@@ -9,6 +9,7 @@ import HighScores from "./features/highscores";
 import Flashcards from "./features/flashcards";
 import { getFlashcards } from "./actions/flashcard/getFlashcards";
 import { getQuizzes } from "./actions/quiz/getQuizzes";
+import AddFlashcard from "./features/addFlashcard";
 
 function App() {
   const dispatch = useDispatch();
@@ -30,6 +31,7 @@ function App() {
           <Route path="/start-quiz" exact component={Quiz} />
           <Route path="/high-scores" exact component={HighScores} />
           <Route path="/flashcards" exact component={Flashcards} />
+          <Route path="/add-flashcard" exact component={AddFlashcard} />
         </Switch>
       </Router>
     </div>
