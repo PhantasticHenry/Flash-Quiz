f8bca97671ae9c7578487353b80aaa829270dc7d
github username <github email address>
2021-01-17 14:04:07 -0800

    add case "ADD_FLASHCARD"

--- frontend-react-redux/src/reducers/flashcardReducer.js
+++ frontend-react-redux/src/reducers/flashcardReducer.js
@@ -2,6 +2,9 @@ export const flashcardReducer = (state = [], action) => {
   switch (action.type) {
     case "FETCH_FLASHCARDS":
       return action.payload;
+
+    case "ADD_FLASHCARD":
+      return [...flashcards, action.payload];
     default:
       return state;
   }
