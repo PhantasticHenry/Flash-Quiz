9af9fca427fe79de110dbfd904b687b53cbf0088
github username <github email address>
2021-01-17 20:40:06 -0800

    quizFlashcardReducer as quizFlashcards

--- frontend-react-redux/src/reducers/index.js
+++ frontend-react-redux/src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import { flashcardReducer as flashcards } from "./flashcardReducer";
 import { quizReducer as quizzes } from "./quizReducer";
+import { quizFlashcardReducer as quizFlashcards } from "./quizFlashcardReducer";
 
 export const rootReducer = combineReducers({
   flashcards,
   quizzes,
+  quizFlashcards,
 });
