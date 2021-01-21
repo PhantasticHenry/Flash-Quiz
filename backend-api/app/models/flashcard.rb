1749be2e54e6407448c467feadb60e3aeffeaa96
github username <github email address>
2021-01-20 22:30:55 -0800

    validate uniqueness and presence

--- backend-api/app/models/flashcard.rb
+++ backend-api/app/models/flashcard.rb
@@ -1,4 +1,6 @@
 class Flashcard < ApplicationRecord
   has_many :quiz_flashcards
   has_many :quizzes, through: :quiz_flashcards
+  validates :category, :question, :correct_answer, :incorrect_answers, presence: true
+  validates :question, uniqueness: true
 end
