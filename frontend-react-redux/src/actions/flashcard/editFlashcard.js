3628f26ca8cb93ee95b4d17ee7da487cd7f85d78
github username <github email address>
2021-01-24 15:41:13 -0800

    PATH request

---
+++ frontend-react-redux/src/actions/flashcard/editFlashcard.js
@@ -0,0 +1,20 @@
+import * as types from "../actionTypes";
+
+export const editFlashcard = (flashcard) => {
+  return (dispatch) => {
+    fetch(types.api + `flashcard/${flashcard}`, {
+      method: "PATCH",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        flashcard,
+      }),
+    })
+      .then((res) => res.json())
+      .then((flashcard) => {
+        flashcard.error
+          ? alert(flashcard.error)
+          : dispatch({ type: types.EDIT_FLASHCARD, payload: flashcard });
+      })
+      .catch((err) => console.log("****", err));
+  };
+};
