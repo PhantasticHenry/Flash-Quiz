3011e0728f2c36dd241152da99acd604e5c66e17
github username <github email address>
2021-01-23 20:09:37 -0800

    remove handleClick function.

--- frontend-react-redux/src/features/flashcards/index.js
+++ frontend-react-redux/src/features/flashcards/index.js
@@ -1,17 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
+import { NavLink } from "react-router-dom";
 import { useSelector } from "react-redux";
 import "./Flashcards.css";
 import Flashcard from "../../components/flashcard";
 
 function Flashcards(props) {
-  const category = props.location.category;
+  const [click, setClick] = useState(false);
   const flashcards = useSelector((state) => state.flashcards);
+  const category = props.location.category;
   const flashcard = flashcards
     .filter((flashcard) => flashcard.category === category)
     .map((flashcard) => <Flashcard flashcard={flashcard} key={flashcard.id} />);
 
   return (
     <div className="flashcards-container">
+      <div className="flashcard-btn">
+        <NavLink className="flashcard-btn add" to="add-flashcard">
+          Add Flashcard
+        </NavLink>
+        <span className="flashcard-btn edit" onClick={() => setClick(!click)}>
+          Edit Flashcard
+        </span>
+        <span className="flashcard-btn remove">Remove Flashcard</span>
+      </div>
       <div className="flashcards">{flashcard}</div>
     </div>
   );
