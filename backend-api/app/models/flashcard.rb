1699eeb0b37930963f3acdb38ef087fe24950c37
github username <github email address>
2021-01-26 18:26:46 -0800

    destroy dependent added

--- backend-api/app/models/flashcard.rb
+++ backend-api/app/models/flashcard.rb
@@ -1,5 +1,5 @@
 class Flashcard < ApplicationRecord
-  has_many :quiz_flashcards
+  has_many :quiz_flashcards, dependent: :destroy
   has_many :quizzes, through: :quiz_flashcards
   validates :category, :question, :correct_answer, :incorrect_answers, presence: true
   validates :question, uniqueness: true
