e8125ee7f76b624b368e0639addd7775e963920d
github username <github email address>
2021-01-28 20:56:32 -0800

    add switch case to edit and remove flashcard

--- frontend-react-redux/src/reducers/flashcardReducer.js
+++ frontend-react-redux/src/reducers/flashcardReducer.js
@@ -6,6 +6,35 @@ export const flashcardReducer = (state = [], action) => {
     case "ADD_FLASHCARD":
       return [...state, action.payload];
 
+    case "EDIT_FLASHCARD": {
+      const {
+        id,
+        category,
+        question,
+        correct_answer,
+        incorrect_answers,
+      } = action.payload;
+
+      const existingFlashcard = state.filter(
+        (flashcard) => flashcard.id === id
+      );
+
+      const updatedFlashcard = {
+        ...existingFlashcard,
+        id: id,
+        category: category,
+        question: question,
+        correct_answer: correct_answer,
+        incorrect_answers: incorrect_answers,
+      };
+
+      const newState = [...state.filter((flashcard) => flashcard.id !== id)];
+      return [...newState, updatedFlashcard];
+    }
+    case "REMOVE_FLASHCARD": {
+      return state.filter((flashcard) => flashcard.id !== action.payload);
+    }
+
     default:
       return state;
   }
