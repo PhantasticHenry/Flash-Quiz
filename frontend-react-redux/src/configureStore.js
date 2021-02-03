c18821f1358aaa8b4d49bab2633f7b8253df2ea4
github username <github email address>
2021-02-02 20:45:33 -0800

    redux persist installed and setup

--- frontend-react-redux/src/configureStore.js
+++ frontend-react-redux/src/configureStore.js
@@ -1,9 +1,23 @@
 import { createStore, applyMiddleware } from "redux";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage"; //defaults to localStorage for web
 import { composeWithDevTools } from "redux-devtools-extension";
 import thunk from "redux-thunk";
 import { rootReducer } from "./reducers";
 
-export const store = createStore(
-  rootReducer,
+const persistConfig = {
+  //configuration object for redux-persist
+  key: "root",
+  storage, //define which storage to use
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const store = createStore(
+  persistedReducer, // pass the persisted reducer instead of rootReducer to createStore
   composeWithDevTools(applyMiddleware(thunk))
 );
+
+const persistor = persistStore(store); // used to create the persist store
+
+export { store, persistor };
