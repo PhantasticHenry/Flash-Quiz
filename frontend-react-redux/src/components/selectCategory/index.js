d5c9a4f16635672cc3a1fd39f45e0313e5c2bebf
github username <github email address>
2021-02-04 00:23:30 -0800

    hide component onClick

--- frontend-react-redux/src/components/selectCategory/index.js
+++ frontend-react-redux/src/components/selectCategory/index.js
@@ -1,13 +1,15 @@
 import React, { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
 import { useSelector } from "react-redux";
 import { Redirect } from "react-router-dom";
 import "./SelectCategory.css";
 import Category from "../category";
 import { renderSwitch } from "../dropdownItems";
+import { setCategory } from "../../actions/category/setCategory";
 
-function SelectCategory({ closeCategories, path }) {
+function SelectCategory({ handleActive, path }) {
+  const dispatch = useDispatch();
   const [categorySelect, setCategorySelect] = useState(false);
-  const [category, setCategory] = useState(undefined);
 
   const categories = useSelector((state) => state.flashcards).map(
     (f) => f.category
@@ -17,19 +19,21 @@ function SelectCategory({ closeCategories, path }) {
 
   const handleClick = (e) => {
     setCategorySelect(true);
-    setCategory(e.target.getAttribute("name"));
+    dispatch(setCategory(e.target.getAttribute("name")));
   };
 
+  const category = useSelector((state) => state.category);
+
   useEffect(() => {
-    category !== undefined && closeCategories();
-  }, [category, closeCategories]);
+    category !== undefined && handleActive();
+  }, [category, handleActive]);
 
   const cat = uniqCat.map((cat, i) => {
     return <Category key={i} category={cat} handleClick={handleClick} />;
   });
 
   return (
-    <div className="select-category">
+    <div className={categorySelect ? "hide" : "select-category"}>
       <h3 className="select">Please Select a Category</h3>
       {cat}
       {categorySelect && (
