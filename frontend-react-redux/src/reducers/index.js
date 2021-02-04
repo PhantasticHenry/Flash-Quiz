f17656cdad5b8d9f7c86999072314ec8879aae31
github username <github email address>
2021-02-04 00:26:26 -0800

    combined category reducer

--- frontend-react-redux/src/reducers/index.js
+++ frontend-react-redux/src/reducers/index.js
@@ -2,9 +2,11 @@ import { combineReducers } from "redux";
 import { flashcardReducer as flashcards } from "./flashcardReducer";
 import { quizReducer as quizzes } from "./quizReducer";
 import { quizFlashcardReducer as quizFlashcards } from "./quizFlashcardReducer";
+import { categoryReducer as category } from "./categoryReducer";
 
 export const rootReducer = combineReducers({
   flashcards,
   quizzes,
   quizFlashcards,
+  category,
 });
