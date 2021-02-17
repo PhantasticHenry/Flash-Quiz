f901be340cf25b2f86be142d7fb86714be697ef4
github username <github email address>
2021-02-16 17:56:45 -0800

    import useState

--- frontend-react-redux/src/hooks/useShuffleArray.js
+++ frontend-react-redux/src/hooks/useShuffleArray.js
@@ -1,4 +1,7 @@
-export function useShuffleArray(answers) {
+import { useState } from "react";
+
+export function useShuffleArray(array) {
+  const [answers] = useState(array);
   let n = answers.length - 1;
   for (; n > 0; n--) {
     const j = Math.floor(Math.random() * (n + 1));
