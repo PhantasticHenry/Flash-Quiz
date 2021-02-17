ae928a921e0c386636085fe79976980f7e117ae8
github username <github email address>
2021-02-17 14:43:10 -0800

    MenuIcon refactor to own component

--- frontend-react-redux/src/components/navbar/index.js
+++ frontend-react-redux/src/components/navbar/index.js
@@ -1,10 +1,9 @@
 import React, { useState } from "react";
-import { FaTimes, FaBars } from "react-icons/fa";
 import { NavLink } from "react-router-dom";
-import "./Navbar.css";
-import logo from "../../images/logo.png";
-import SelectCategory from "../selectCategory";
 import MenuIcon from "./MenuIcon";
+import NavMenu from "./NavMenu";
+import NavLogo from "./NavLogo";
+import "./Navbar.css";
 
 function Navbar() {
   const [clicked, setClicked] = useState(false);
@@ -55,53 +54,20 @@ function Navbar() {
 
   return (
     <nav className="navbar">
-      <NavLink to="/" onClick={handleClick}>
-        <img className="navbar-logo" src={logo} alt="logo" />
-      </NavLink>
+      <NavLogo handleClick={handleClick} />
       <MenuIcon clicked={clicked} setClicked={setClicked} />
-      <ul className={clicked ? "nav-menu active" : "nav-menu"}>
-        <li className="nav-item-home">
-          <NavLink className="nav-link" onClick={() => handleClick()} to="/">
-            Home
-          </NavLink>
-        </li>
-        <li className="nav-item">
-          <NavLink
-            to="/"
-            className={quizActive ? "nav-link selected" : "nav-link"}
-            onClick={(e) => handlePath(e)}
-          >
-            Start Quiz
-          </NavLink>
-        </li>
-        <li className="nav-item">
-          <NavLink
-            to="/high-scores"
-            className="nav-link"
-            activeClassName="selected hs"
-            onClick={() => handleClick()}
-          >
-            High Scores
-          </NavLink>
-        </li>
-        <li className="nav-item">
-          <NavLink
-            className={flashcardsActive ? "nav-link fc" : "nav-link"}
-            to="/"
-            onClick={(e) => handlePath(e)}
-          >
-            Flashcards
-          </NavLink>
-        </li>
-        {categories && (
-          <SelectCategory
-            handleActive={handleActive}
-            path={path}
-            closeDiv={() => setClicked(false)}
-          />
-        )}
-        {sidebar()}
-      </ul>
+      <NavMenu
+        clicked={clicked}
+        handleClick={handleClick}
+        quizActive={quizActive}
+        handlePath={handlePath}
+        flashcardsActive={flashcardsActive}
+        categories={categories}
+        handleActive={handleActive}
+        path={path}
+        setClicked={setClicked}
+        sidebar={sidebar}
+      />
     </nav>
   );
 }
