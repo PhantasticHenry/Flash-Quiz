1baab0363874b0e77e453f10a1f1c3d39ab6be34
github username <github email address>
2021-11-10 22:04:15 -0800

    added containers

--- frontend-react-redux/src/features/quiz/index.js
+++ frontend-react-redux/src/features/quiz/index.js
@@ -6,58 +6,70 @@ import Question from "../../components/question";
 import Result from "../../components/result";
 
 function Quiz() {
-  const category = useSelector((state) => state.category);
-  const flashcards = useSelector((state) => state.flashcards);
+	const category = useSelector((state) => state.category);
+	const flashcards = useSelector((state) => state.flashcards);
 
-  const [index, setIndex] = useState(0);
-  const [correct, setCorrect] = useState(0);
-  const [incorrect, setIncorrect] = useState(0);
-  const [gameFinished, setGameFinished] = useState(false);
+	const [index, setIndex] = useState(0);
+	const [correct, setCorrect] = useState(0);
+	const [incorrect, setIncorrect] = useState(0);
+	const [gameFinished, setGameFinished] = useState(false);
 
-  const questions = flashcards
-    .filter((flashcard) => flashcard.category === category)
-    .sort(() => Math.random() - 0.5);
+	const questions = flashcards
+		.filter((flashcard) => flashcard.category === category)
+		.sort(() => Math.random() - 0.5);
 
-  let question = questions[index].question;
-  let correct_answer = questions[index].correct_answer;
-  let incorrect_answers = questions[index].incorrect_answers;
-  let answers = [...incorrect_answers, correct_answer];
+	let question = questions[index].question;
+	let correct_answer = questions[index].correct_answer;
+	let incorrect_answers = questions[index].incorrect_answers;
+	let answers = [...incorrect_answers, correct_answer];
 
-  const nextQuestion = () => {
-    setIndex(index + 1);
-  };
+	const nextQuestion = () => {
+		setIndex(index + 1);
+	};
 
-  const checkAnswer = (answer) => {
-    if (answer === correct_answer) {
-      setCorrect(correct + 1);
-    } else {
-      setIncorrect(incorrect + 1);
-    }
-  };
+	const checkAnswer = (answer) => {
+		if (answer === correct_answer) {
+			setCorrect(correct + 1);
+		} else {
+			setIncorrect(incorrect + 1);
+		}
+	};
 
-  const handleClick = (e) => {
-    if (index === questions.length - 1) {
-      setGameFinished(true);
-    } else {
-      checkAnswer(e.currentTarget.getAttribute("name"));
-      nextQuestion();
-    }
-  };
+	const handleClick = (e) => {
+		if (index === questions.length - 1) {
+			setGameFinished(true);
+		} else {
+			checkAnswer(e.currentTarget.getAttribute("name"));
+			nextQuestion();
+		}
+	};
 
-  return (
-    <div className="quiz-container">
-      {gameFinished ? (
-        <>
-          <Result correct={correct} incorrect={incorrect} category={category} />
-        </>
-      ) : (
-        <>
-          <Question question={question} />
-          <Answers answers={answers} handleClick={handleClick} />
-        </>
-      )}
-    </div>
-  );
+	return (
+		<div className="quiz-container">
+			{gameFinished ? (
+				<>
+					<Result
+						correct={correct}
+						incorrect={incorrect}
+						category={category}
+					/>
+				</>
+			) : (
+				<>
+					<div className="questions">
+						<Question question={question} />
+					</div>
+					<div className="answers">
+						<Answers answers={answers} handleClick={handleClick} />
+					</div>
+				</>
+				// <>
+				//   <Question question={question} />
+				//   <Answers answers={answers} handleClick={handleClick} />
+				// </>
+			)}
+		</div>
+	);
 }
 
 export default Quiz;
